adaaa8bfd3665088b0604a3e90286203c7541937
Jagravi Patel <jpatel23@confederationcollege.ca>
2021-08-18 17:43:01 -0400

    todo and ss

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,7 +12,9 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
+            var total_2020_precipitation = measurements.Where(m => m.year == 2020)
+                                        .Select(m => m.precipitation)
+                                        .Sum();
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
             //
@@ -20,13 +22,16 @@ namespace WeatherLab
             //   see: https://en.wikipedia.org/wiki/Heating_degree_day
             //
 
-            // ?? TODO ??
-
+            var days = measurements.GroupBy(y => y.year)
+                   .Select(sel => new
+                   {
+                       year = sel.Key,
+                       hdd = sel.Where(hdd => hdd.meantemp < 18).Count(),
+                       cdd = sel.Where(cdd => cdd.meantemp >= 18).Count()
+                   });
             //
             // Cooling degree days have a mean temp of >=18C
             //
-
-            // ?? TODO ??
 
             //
             // Most Variable days are the days with the biggest temperature
@@ -42,12 +47,29 @@ namespace WeatherLab
             //
             Console.WriteLine("Year\tHDD\tCDD");
 
-            // ?? TODO ??
+            foreach (var d in days)
+            {   Console.WriteLine($"{ d.year }\t{ d.hdd }\t{ d.cdd }");     }
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
             // ?? TODO ??
+            var display = measurements.Select(sel => new
+                    {
+                        date = $"{sel.year}-{sel.month:d2}-{sel.day:d2}",
+                        delta = (sel.maxtemp - sel.mintemp)
+                    })
+                    .OrderByDescending(d => d.delta);
+
+            int check = 0;
+            foreach (var i in display)
+            {   if (check < 5) {
+                    Console.WriteLine($"{i.date}\t{i.delta}");
+                    check++;
+                }
+                else
+                { break; }
+            }
         }
     }
 }
